932c8bb3f2b7095302a39c7049456845bc9bf337
robertlassila <rlassila42@gmail.com>
2025-05-30 17:53:25 -0400

    Took out unessecary import

--- src/main/java/com/coderscampus/assignment14/repository/ChannelRepository.java
+++ src/main/java/com/coderscampus/assignment14/repository/ChannelRepository.java
@@ -1,7 +1,6 @@
 package com.coderscampus.assignment14.repository;
 
 import com.coderscampus.assignment14.domain.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
